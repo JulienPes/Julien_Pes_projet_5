be5d07a0bd12297d733384072befeb2bbb7c4db3
Julien <juju2nizza@gmail.com>
2022-08-04 18:04:18 +0200

    new prod

--- front/js/product.js
+++ front/js/product.js
@@ -13,6 +13,97 @@ const getProduct = async (prod) => {
     const prodApi = `http://localhost:3000/api/products/${prod}`;
     const response = await fetch(prodApi);
     if (!response.ok) {
+      throw new Error(`"erreur http :" ${response.status}`);
+    } else {
+      const products = await response.json();
+      console.log(products);
+      displayProd(products);
+    }
+  } catch (error) {
+    return {
+      error: true,
+      msg: error,
+    };
+  }
+};
+
+const displayProd = (prod) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", prod.imageUrl);
+  img.setAttribute("alt", prod.altTxt);
+
+  document.getElementsByClassName("item__img")[0].appendChild(img);
+  document.getElementById("title").innerHTML = prod.name;
+  document.getElementById("price").innerHTML = prod.price;
+  document.getElementById("description").textContent = prod.description;
+
+  prod.colors.forEach((color) => {
+    let opt = new Option(color, color);
+    document.getElementById("colors").appendChild(opt);
+  });
+  attach(prod._id);
+};
+
+const attach = (id) => {
+  document.getElementById("addToCart").addEventListener("click", () => {
+    add(id);
+  });
+};
+
+const add = (id) => {
+  const quanty = document.getElementById("quantity").value;
+  let color = document.getElementById("colors");
+  const opt = color.options[color.selectedIndex].value;
+  console.log(checkQuant(quanty));
+  console.log(checkSelected(opt));
+  if (checkQuant(quanty) && checkSelected(opt)) {
+    const but = (document.getElementById("addTocart").disabled = true);
+  }
+};
+
+const checkQuant = (quanty) => {
+  if (
+    "undefined" === typeof quanty ||
+    0 >= parseInt(quanty) ||
+    100 < parseInt(quanty)
+  ) {
+    alert("Vous devez selectionner une quantité entre 1 et 100 !");
+    document.getElementById("quantity").value = 1;
+    return false;
+  }
+  return true;
+};
+const checkSelected = (optSelected) => {
+  if (!optSelected) {
+    alert("Vous devez choisir une couleur !");
+    return false;
+  }
+  return true;
+};
+
+// const createProd = (prod) => {
+//   let img = document.createElement("img");
+//   img.setAttribute("src", prod.imageUrl);
+//   img.setAttribute("alt", prod.altTxt);
+
+//   document.getElementsByClassName("item__img")[0].appendChild(img);
+//   document.getElementById("price").textContent = prod.price;
+//   document.getElementById("title").textContent = prod.name;
+//   document.getElementById("description").textContent = prod.description;
+//   prod.colors.forEach((color) => {
+//     let option = new Option(color, color);
+//     document.getElementById("colors").appendChild(option);
+
+//     attach(prod._id)
+//   });
+// };
+
+/*
+const getProduct = async (prod) => {
+  try {
+    const prodApi = `http://localhost:3000/api/products/${prod}`;
+    const response = await fetch(prodApi);
+    if (!response.ok) {
       throw new error(`"erreur http :" ${response.status}`);
     } else {
       const product = await response.json();
@@ -36,7 +127,28 @@ const createProd = (prod) => {
   document.getElementById("price").textContent = prod.price;
   document.getElementById("title").textContent = prod.name;
   document.getElementById("description").textContent = prod.description;
+  prod.colors.forEach((color) => {
+    let option = new Option(color, color);
+    document.getElementById("colors").appendChild(option);
+
+    attach(prod._id)
+  });
 };
+
+const attach = (prod) => {
+  document.getElementById("addToCart").addEventListener("click" , () =>{
+addProd(prod)
+  })  
+};
+const addProd = (prod) =>{
+ const quant =  document.getElementById("quantity").value
+ let selectOpt = document.getElementById("colors")
+ const opt = selectOpt.options[selectOpt.selectedIndex].value
+ console.log(quant);
+ console.log(opt);
+}
+
+ 
 
 /*
 const getProduct = async (zeub) => {
--- front/js/script.js
+++ front/js/script.js
@@ -2,25 +2,27 @@ window.onload = () => {
   console.log("Dom is loaded");
   getProducts();
 };
-
 const getProducts = async () => {
   try {
     const urlApi = "http://localhost:3000/api/products";
     const response = await fetch(urlApi);
     if (!response.ok) {
       throw new Error(`"erreur http :" ${response.status}`);
+    } else {
+      const products = await response.json();
+      for (let product of products) {
+        create(product);
+      }
     }
-    const products = await response.json();
-
-    for (let product of products) {
-      create(product);
-    }
-  } catch (e) {
-    console.log("Error", e);
+  } catch (error) {
+    return {
+      error: true,
+      msg: error,
+    };
   }
 };
 
-function create(prod) {
+const create = (prod) => {
   let link = document.createElement("a");
   link.setAttribute("href", `./product.html?id=${prod._id}`);
 
@@ -29,7 +31,7 @@ function create(prod) {
   let img = document.createElement("img");
   img.setAttribute("src", prod.imageUrl);
   img.setAttribute("alt", prod.altTxt);
-  
+
   let h3 = document.createElement("h3");
   h3.setAttribute("class", "productName");
   h3.textContent = prod.name;
@@ -42,4 +44,4 @@ function create(prod) {
   link.append(article);
 
   document.getElementById("items").append(link);
-}
+};
