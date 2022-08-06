83b1b1885b624404fcaac36d7286def803983916
Julien <juju2nizza@gmail.com>
2022-08-06 17:53:57 +0200

    new

--- front/js/product.js
+++ front/js/product.js
@@ -3,50 +3,20 @@ window.onload = () => {
   getId();
 };
 // Récup ID
-
 const getId = () => {
   const urlId = new URLSearchParams(window.location.search).get("id");
-  getProd(urlId);
+  console.log(urlId);
+  getData(urlId);
 };
 
-/*
-const getId = () => {
-  let firstArg = new URLSearchParams(window.location.search).get("id");
-  console.log(firstArg);
-  getProduct(firstArg);
-};
-*/
-// Contact serveur
-const getProd = async (id) => {
+const getData = async (id) => {
   try {
-    const getUrl = `http://localhost:3000/api/products/${id}`;
-    const response = await fetch(getUrl);
-    if (!response) {
-      throw new Error(`"erreur http " : ${response.status}`);
-    }
-    const prod = await response.json();
-    console.log(prod);
-    displayProd(prod);
-  } catch (error) {
-    return {
-      error: true,
-      msg: error,
-    };
-  }
-};
-
-/*
-
-const getProduct = async (prod) => {
-  try {
-    const prodApi = `http://localhost:3000/api/products/${prod}`;
-    const response = await fetch(prodApi);
-    if (!response.ok) {
-      throw new Error(`"erreur http :" ${response.status}`);
-    } else {
-      const products = await response.json();
-      console.log(products);
-      displayProd(products);
+    const urlApi = `http://localhost:3000/api/products/${id}`;
+    const response = await fetch(urlApi);
+    if (response.ok) {
+      const prod = await response.json();
+      console.log(prod);
+      displayProd(prod);
     }
   } catch (error) {
     return {
@@ -55,132 +25,57 @@ const getProduct = async (prod) => {
     };
   }
 };
-*/
-// Affiche les produits
 
-const displayProd = (prodCard) => {
-  const img = document.createElement("img");
-  img.setAttribute("src", prodCard.imageUrl);
-  img.setAttribute("alt", prodCard.altTxt);
-  document.getElementsByClassName("item__img")[0].appendChild(img);
-  document.getElementById("title").textContent = prodCard.name;
-  document.getElementById("price").textContent = prodCard.price;
-  document.getElementById("description").textContent = prodCard.description;
-
-  prodCard.colors.forEach((color) => {
-    const opt = new Option(color, color);
-    document.getElementById("colors").appendChild(opt);
-  });
-  attach(prodCard._id);
-};
-
-/*
 const displayProd = (prod) => {
   let img = document.createElement("img");
   img.setAttribute("src", prod.imageUrl);
   img.setAttribute("alt", prod.altTxt);
-
   document.getElementsByClassName("item__img")[0].appendChild(img);
-  document.getElementById("title").innerHTML = prod.name;
-  document.getElementById("price").innerHTML = prod.price;
+  document.getElementById("title").textContent = prod.name;
+  document.getElementById("price").textContent = prod.price;
   document.getElementById("description").textContent = prod.description;
-
   prod.colors.forEach((color) => {
     let opt = new Option(color, color);
     document.getElementById("colors").appendChild(opt);
   });
   attach(prod._id);
 };
-*/
-// Ajoute un écouteur
 
-const attach = (id) => {
+const attach = (prod) => {
   document.getElementById("addToCart").addEventListener("click", () => {
-    add(id);
+    add(prod);
   });
 };
 
-const add = (prod) => {
-  let color = document.getElementById("colors");
-  const selectedColor = color.options[color.selectedIndex].value;
-  const selectedQty = document.getElementById("quantity").value;
-  console.log(checkColor(selectedColor));
-  console.log(checkOpt(selectedQty));
-  if (checkOpt(selectedQty) && checkColor(selectedColor)) {
-    const but = (document.getElementById("addToCart").disabled = true);
-    let order = {
-      Identifiant: prod,
-      Couleur: selectedColor,
-      Quantité: selectedQty,
+const add = (product) => {
+  let colorOpt = document.getElementById("colors");
+  const clrValue = colorOpt.options[colorOpt.selectedIndex].value;
+  let qtyValue = document.getElementById("quantity").value;
+
+  if (clrChoice(clrValue) && qtyChoice(qtyValue)) {
+    let btn = (document.getElementById("addToCart").disabled = true);
+    let prd = {
+      _id: product,
+      color: clrValue,
+      quantity: qtyValue,
     };
-    console.log(order);
+    let objLinea = JSON.stringify(prd);
+    localStorage.setItem("obj", objLinea);
+    console.log(objLinea);
   }
 };
 
-const checkOpt = (selectedQty) => {
-  if (
-    "undefined" === typeof selectedQty ||
-    0 >= selectedQty ||
-    100 < selectedQty
-  ) {
-    alert("Veuillez choisir une quantité !");
-    return false;
-  }
-  return true;
-};
-const checkColor = (selectedColor) => {
-  if (!selectedColor) {
+const clrChoice = (clrValue) => {
+  if (!clrValue) {
     alert("Veuillez choisir une couleur !");
     return false;
   }
   return true;
 };
-
-/*
-const attach = (id) => {
-  document.getElementById("addToCart").addEventListener("click", () => {
-    add(id);
-  });
-};
-*/
-// Valide le panier
-
-/*
-const add = (id) => {
-  const quanty = document.getElementById("quantity").value;
-  let color = document.getElementById("colors");
-  const opt = color.options[color.selectedIndex].value;
-  console.log(checkQuant(quanty));
-  console.log(checkSelected(opt));
-  if (checkQuant(quanty) && checkSelected(opt)) {
-    const but = (document.getElementById("addTocart").disabled = true);
-  }
-};
-*/
-// Vérifie la quantité
-
-/*
-const checkQuant = (quanty) => {
-  if (
-    "undefined" === typeof quanty ||
-    0 >= parseInt(quanty) ||
-    100 < parseInt(quanty)
-  ) {
-    alert("Vous devez selectionner une quantité entre 1 et 100 !");
-    document.getElementById("quantity").value = 1;
+const qtyChoice = (qtyValue) => {
+  if (qtyValue === "undefined" || 0 >= qtyValue || 100 < qtyValue) {
+    alert("Veuillez choisir la quantité !");
     return false;
   }
   return true;
 };
-*/
-// Vérifie la couleur
-
-/*
-const checkSelected = (optSelected) => {
-  if (!optSelected) {
-    alert("Vous devez choisir une couleur !");
-    return false;
-  }
-  return true;
-};
-*/
